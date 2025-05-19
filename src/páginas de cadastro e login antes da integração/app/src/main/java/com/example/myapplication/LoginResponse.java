1c78eabbcc4d07a3a3df8467591d50f772e11e89
Arthur <arthurf.alves02@gmail.com>
2025-05-18 22:45:53 -0300

    teste

---
+++ src/páginas de cadastro e login antes da integração/app/src/main/java/com/example/myapplication/LoginResponse.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+
+public class LoginResponse {
+    private boolean sucesso;
+    private Usuario usuario;
+
+    public boolean isSucesso() {
+        return sucesso;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+}
+
